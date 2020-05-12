659fd131d5ed0eff29d45cce893557342de14e6f
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-05-12 16:24:32 +0000

    Gitignore updated

--- env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["MONGO_URI"] = "mongodb+srv://krisztinatxt:4l4kul4mol3kul4@myfirstcluster-wrvet.mongodb.net/lockdown_kitchen?retryWrites=true"
-os.environ["MONGO_DBNAME"] = "lockdown_kitchen"
