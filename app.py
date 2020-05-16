00ce33a8024c971b4ff3ae08e4ba5dce3954cd94
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-05-16 15:18:10 +0000

    Edit recipe function added, wired up with app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from os import path
 from flask import Flask, render_template, redirect, request, url_for
 if path.exists("env.py"):
     import env
+from bson.objectid import ObjectId
+
 #Define the app, and set the MONGO_URI
 app = Flask(__name__)
 
@@ -31,6 +33,14 @@ def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
+
+#Find a recipe by its ID then render the editrecipe HTML file
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)
 
 #Host and Port set
 if __name__ == '__main__':
