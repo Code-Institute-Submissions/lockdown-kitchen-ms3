1d7ac108c7cc5b66b0b23839dd06ab88e0f59225
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-05-18 16:25:39 +0000

    Favicon link changed ti url_for, favicon loaded

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+ $(document).ready(function() {
+         //When the document is ready, the user is able to select a category before submiting the recipe
+         $('select').material_select();
+         $(".button-collapse").sideNav();
+         $('.modal').modal();
+         });
