a45af3d82f19ee0462808cbe68557559a51dd949
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-05-23 12:00:31 +0000

    Category dropdown menu fixed in mobile version by adding all_categories list

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def find_recipes():
 #Find all of the recipes
 @app.route('/get_recipes')
 def get_recipes():
-    all_categories =  mongo.db.categories.find()
+    all_categories = list(mongo.db.categories.find())
     return render_template("recipes.html", categories=all_categories, recipes=mongo.db.recipes.find())
 
 #Add new recipe using a form, submiting it using POST method
