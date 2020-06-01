053ebc90a630aac7b02c9a0cacfe6bf1cf3b7eff
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-06-01 09:58:05 +0000

    JavaScript files comments added

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,8 +1,9 @@
+//Defining variables for the form and succes/error messages
 var success = document.querySelector("#modal2");
 var modalClose = document.querySelector("#modal1");
 var fail = document.querySelector("#modal3")
 
-
+//Sendmail function to redirect the provided data to my email address
 function sendMail(contactForm) {
     emailjs.init("user_UW97WmP3GsBepuyB8Vffd");
     emailjs.send("gmail", "lockdown_kitchen", {
@@ -10,6 +11,7 @@ function sendMail(contactForm) {
         "from_email": contactForm.emailaddress.value,
         "feedback": contactForm.feedback.value
     })
+    //Succes message with timeout, the message is not displayed by default
     .then(
         function(response) {
             console.log("SUCCESS", response);
@@ -19,7 +21,7 @@ function sendMail(contactForm) {
             success.style.display = "none";
             }, 3500);
         },
-        
+        //Error message with my email address, timeout set to 5000 to let the user to copy the email address
         function(error) {
              console.log("FAILED", error);
              modalClose.style.display = "none";
